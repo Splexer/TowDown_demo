416463681eb965e83f71ec17d656ea2a5cdbeef6
Splexer <splexer333@gmail.com>
2025-09-30 20:48:47 +0700

    add base HUD system and strt using Events

--- core/Global.gd
+++ core/Global.gd
@@ -2,10 +2,18 @@ extends Node
 
 var player_walk_speed : float = 60.0
 var player_sprint_speed : float = 120.0
-var player_hp : int = 1
+var player_hp : int = 4
+
+var on_level_coins : int = 20
+var collected_coins : int = 0
+var required_coins : int = 10
 
 var rng := RandomNumberGenerator.new()
 
 func _ready() -> void:
+	Events.pick_up_coin.connect(_pick_up_coin)
 	rng.randomize()
 	seed(rng.seed)
+
+func _pick_up_coin()-> void:
+	collected_coins += 1
